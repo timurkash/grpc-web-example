5fbc2c48a4175259a2a1340be2dfde81314609f1
tkashaev <tkashaev@industrial.market>
2022-04-14 17:40:27 +0300

    modified to envoy:v1.21.1

---
+++ 1.sh
@@ -0,0 +1,6 @@
+curl 'http://localhost:8080/smpl.time.api.v1.TimeService/GetCurrentTime' \
+-X POST \
+-H 'Accept: application/grpc-web-text' \
+-H 'Content-Type: application/grpc-web-text' \
+--data-raw 'AAAAAAA='
+
